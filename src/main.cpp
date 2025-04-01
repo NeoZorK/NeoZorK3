faf3f5805c297eff892c5caeb9a9cd26171250d5
Ростислав Щербина <neozork@protonmail.com>
2025-04-01 18:19:39 +0300

    add time measure

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
+#include <chrono>
 #include "main.h" // Подключаем наш пустой заголовок
 
 int main(int argc, char* argv[]) {
+    
+    auto start_time = std::chrono::high_resolution_clock::now();
+
     // Подавляем предупреждения о неиспользуемых параметрах
     (void)argc;
     (void)argv;
 
     std::cout << "NeoZorK3 Minimal Build - OK" << std::endl;
+    
+    auto end_time = std::chrono::high_resolution_clock::now();
+
+      auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
+
+      std::cout << "Время работы программы: " << duration << " мс" << std::endl;
+
     return 0;
 }
