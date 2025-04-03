35f516d8276f43f58de4d4d53e55801429a54ef7
Ростислав Щербина <neozork@protonmail.com>
2025-04-03 21:06:52 +0300

    version 0.1.7

--- include/version.h
+++ include/version.h
@@ -6,7 +6,7 @@
 // Version of the program
 namespace neozork {
     // C++17: inline variable
-    inline const std::string PROGRAM_VERSION = "v0.1.6";
+    inline const std::string PROGRAM_VERSION = "v0.1.7";
 }
 
 #endif // NEOZORK3_VERSION_H
